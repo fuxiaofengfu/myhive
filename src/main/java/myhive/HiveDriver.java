54a3ebcd549e4e2b5f7d71bc523f27e6504117da
xiaofengfu <fuxiaofeng1107@163.com>
2017-11-27 17:56:26 +0800

    init hive

---
+++ src/main/java/myhive/HiveDriver.java
@@ -0,0 +1,17 @@
+package myhive;
+
+import org.apache.hadoop.hive.cli.CliDriver;
+
+public class HiveDriver {
+
+	public static void main(String[] args) {
+		int ret = 0;
+		try {
+			System.setProperty("jline.WindowsTerminal.directConsole","false");
+			ret = new CliDriver().run(args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.exit(ret);
+	}
+}
